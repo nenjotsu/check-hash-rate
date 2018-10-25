79c4d123bd79d34f235224610ccaa8fc1e9cb41b
Nenjo Tsu <renenjo.valente@gmail.com>
2018-10-25 22:38:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+
+import sys, time
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 import urllib3
@@ -5,81 +8,47 @@ import json
 import heapq
 import os
 
+is_should_stop = False
 
-# just holds a function, its arguments, and when we want it to execute.
-class TimeoutFunction:
-    def __init__(self, function, timeout, *args):
-        self.function = function
-        self.args = args
-        self.startTime = datetime.now() + timedelta(0, 0, 0, timeout)
+class RetryError(Exception):
+    pass
 
-    def execute(self):
-        self.function(*self.args)
-
-
-# A "todo" list for all the TimeoutFunctions we want to execute in the future
-# They are sorted in the order they should be executed, thanks to heapq
-class TodoList:
-    def __init__(self):
-        self.todo = []
-
-    def addToList(self, tFunction):
-        heapq.heappush(self.todo, (tFunction.startTime, tFunction))
-
-    def executeReadyFunctions(self):
-        if len(self.todo) > 0:
-            tFunction = heapq.heappop(self.todo)[1]
-            while tFunction and datetime.now() > tFunction.startTime:
-                #execute all the functions that are ready
-                tFunction.execute()
-                if len(self.todo) > 0:
-                    tFunction = heapq.heappop(self.todo)[1]
-                else:
-                    tFunction = None
-            if tFunction:
-                #this one's not ready yet, push it back on
-                heapq.heappush(self.todo, (tFunction.startTime, tFunction))
-
-
-def notify(title, text):
-    os.system("""
-              osascript -e 'display notification "{}" with title "{}"'
-              """.format(text, title))
-
-
-def singleArgFunction(x):
+def is_down():
     http = urllib3.PoolManager()
-    url = 'https://api.nanopool.org/v1/eth/reportedhashrate/0x6ab7d21de169cbfc81afb1e5f1cd79d3e373e9ef'
+    url = 'https://api.nanopool.org/v1/eth/reportedhashrate/<YOUR-ETH-WALLET>'
     response = http.request('GET', url)
     contents = BeautifulSoup(response.data)
     newDictionary = json.loads(str(contents))
-    if newDictionary['status'] == False:
-        print 'Miner is Down or you do not have internet'
+    if newDictionary['data'] == 0:
+        is_should_stop = True
         os.system("shutdown /r /t 1")
-    if newDictionary['status'] == True:
-        notify("Nanopool", "Miner is Running")
+    if newDictionary['data'] > 0:
+        # notify("Nanopool", "Miner is Running")
         print 'Miner is Running'
+        print str(datetime.now())
         print 'Status'
         print str(newDictionary['status'])
         print 'Hashrate'
         print str(newDictionary['data'])
+        is_should_stop = False
 
 
-def multiArgFunction(x, y):
-    #Demonstration of passing multiple-argument functions
-    print '==> End'
+def retryloop(attempts, timeout):
+    starttime = time.time()
+    success = set()
+    for i in range(attempts):
+        success.add(True)
+        yield success.clear
+        if success:
+            return
+        if starttime + timeout < time.time():
+            break
+    raise RetryError
 
 
-# Make some TimeoutFunction objects
-# timeout is in milliseconds
-a = TimeoutFunction(singleArgFunction, 600000, 20)
-# b = TimeoutFunction(multiArgFunction, 2000, *(11, 12))
-# c = TimeoutFunction(quit, 3000, None)
+for retry in retryloop(1000, timeout=60):
+    is_down()
+    time.sleep(600)
 
-todoList = TodoList()
-todoList.addToList(a)
-# todoList.addToList(b)
-# todoList.addToList(c)
-
-while True:
-    todoList.executeReadyFunctions()
+    if is_should_stop == False:
+        retry()
